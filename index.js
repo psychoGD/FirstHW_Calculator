d702240b8c0cc2e4dcc71763d59969200235995d
Huseyn Abbasov <nelbekistekan12@gmail.com>
2023-03-08 02:30:15 +0400

    Full

--- index.js
+++ index.js
@@ -3,16 +3,49 @@ function show(content){
     mainh1.style.color = "white"
     mainh1.append(content)
 }
-
+var selectedOperation = false;
 function SetAllButtonOnClick(){
     let buttons = document.getElementsByTagName("button")
 
     for (let i = 0; i < buttons.length; i++) {
-        
-        buttons[i].addEventListener("click",function(){
-            var content = buttons[i].textContent;
-            show(content);
-        });
+        let inner = buttons[i].innerHTML
+        if(inner == "="){
+            buttons[i].addEventListener("click",function(){
+                mainh1.innerHTML = eval(mainh1.innerHTML)
+                selectedOperation = true
+
+            });
+        }
+        else if(inner == "+" || inner == "-" || inner == "*" || inner == "/"){
+            buttons[i].addEventListener("click",function(){
+                if(selectedOperation){
+
+                    selectedOperation = false
+                    mainh1.innerHTML += inner
+                }
+            });
+        }
+        else if(buttons[i].innerHTML == "AC"){
+            buttons[i].addEventListener("click",function(){
+                mainh1.innerHTML = ""
+                selectedOperation = true
+            });
+        }
+        else if(buttons[i].innerHTML == "C"){
+            buttons[i].addEventListener("click",function(){
+                mainh1.innerHTML = ""
+                selectedOperation = true
+
+            });
+        }
+        else{
+            buttons[i].addEventListener("click",function(){
+                var content = buttons[i].textContent;
+                show(content);
+                selectedOperation = true
+
+            });
+        }
     }
 
     
